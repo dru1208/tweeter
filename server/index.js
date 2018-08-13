73ec9faf1d16979e1dd7ced0e5c9706877064f84
Andrew Hsieh <andrewhsieh1208@gmail.com>
2018-08-13 21:41:22 +0000

    first day activities

--- server/index.js
+++ server/index.js
@@ -29,6 +29,11 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
 
+// tweet submission post
+// app.post("/tweets/", (req, res) => {
+
+// })
+
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
